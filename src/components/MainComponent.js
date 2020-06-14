859e2ea585427a60b14e9b3d54a89ea2fab3fadf
twilight-warlock <devansh.ts@somaiya.edu>
2020-06-15 02:43:12 +0530

    Added new formatted data in shared folder along with updating HomeComponent,created new contact component .........Single Page App pt1..........

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -3,8 +3,13 @@ import Header from "./HeaderComponent";
 import Footer from "./FooterComponent";
 import Menu from "./MenuComponent";
 import Home from "./HomeComponent";
+import Contact from "./ContactComponentt";
 // import DishdetailComponent from "./DishdetailComponent";
 import { DISHES } from "../shared/dishes";
+import { COMMENTS } from "../shared/comments";
+import { PROMOTIONS } from "../shared/promotions";
+import { LEADERS } from "../shared/leaders";
+
 import { Switch, Redirect, Route } from "react-router-dom";
 
 class Main extends Component {
@@ -12,11 +17,22 @@ class Main extends Component {
     super(props);
     this.state = {
       dishes: DISHES,
+      comments: COMMENTS,
+      promotions: PROMOTIONS,
+      leaders: LEADERS,
     };
   }
 
   render() {
-    const HomePage = () => <Home />;
+    const HomePage = () => (
+      <Home
+        dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+        promotion={
+          this.state.promotions.filter((promotion) => promotion.featured)[0]
+        }
+        leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+      />
+    );
 
     return (
       <div>
@@ -28,6 +44,7 @@ class Main extends Component {
             path="/menu"
             component={() => <Menu dishes={this.state.dishes} />}
           />
+          <Route exact path="/contactus" component={Contact} />
           <Redirect to="/home" />
         </Switch>
         <Footer />
